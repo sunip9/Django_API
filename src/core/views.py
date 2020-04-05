2fbacb4acae19fcbe0970f8bf97048a9f7baf9f1
unknown <Sunip@Sunip.ad.nttml.com>
2020-04-05 08:54:33 +0600

    django setup

---
+++ src/core/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+from django.shortcuts import render
+
+def test_view(request):
+    data = {
+        'name': 'Sunip',
+        'age': 30
+    }
+    return JsonResponse(data)
